820f4e607ad0ab796be2f94f2029c7f630379f7a
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2023-12-07 08:04:22 +0200

    Add solution for Day 7

--- days/7/Day7.h
+++ days/7/Day7.h
@@ -25,7 +25,7 @@ public:
     return "7";
   }
 
-  LL DoWork1()
+  LL DoWork(bool part2)
   {
     LL ret = 0;
     map<char, LL> cm;
@@ -45,44 +45,104 @@ public:
     cm['3'] = ttt--;
     cm['2'] = ttt--;
 
-    auto getHandValue = [](string h) 
+    if (part2)
+      cm['J'] = ttt--;
+
+    auto getHandValue = [part2](string h) 
       {
-        map<char, int> freq;
-        for (char c : h)
-          freq[c]++;
+        assert(h.size() == 5);
+        unordered_map<char, int> freq;
+        for (auto c : h)
+        {
+          if (c != '\0')
+            freq[c]++;
+        }
         vector<pair<char, int>> freqV;
         for (auto [k, v] : freq)
         {
           freqV.push_back(make_pair(k, v));
         }
 
-        int ret = 0;
-        //if (h[0] == h[1] == h[2] == h[3] == h[4])
-        if (freqV.size() == 1)
-          ret = 7; // five of a kind
-        if (freqV.size() == 2)
-        {
-          if (freqV[0].second == 4 || freqV[1].second == 4)
-            ret = 6; // four of a kind
-          if (freqV[0].second == 3 || freqV[1].second == 3)
-            ret = 5; // full house
-        }
-        if (freqV.size() == 3)
-        {
-          if (freqV[0].second == 3 || freqV[1].second == 3 || freqV[2].second == 3)
-            ret = 4; // three of akind
 
-          if (freqV[0].second == 2 || freqV[1].second == 2 || freqV[2].second == 2)
-            ret = 3; // two pair
-        }
-        if (freqV.size() == 4)
+        int ret = 0;
+
+        
+        if (part2 && freq['J'] == 1)
         {
-          if (freqV[0].second == 2 || freqV[1].second == 2 || freqV[2].second == 2 || freqV[3].second == 2)
-            ret = 2;; // one pair
+          if (freqV.size() == 2)
+            ret = 7; // five of a kind
+          else if (freqV.size() == 3)
+          {
+            if (freqV[0].second == 3 || freqV[1].second == 3 || freqV[2].second == 3)
+              ret = 6; // four of a kind
+            else
+              ret = 5; // full house
+          }
+          else if (freqV.size() == 4)
+            ret = 4; // three of akind
+          else if (freqV.size() == 5)
+            ret = 2; // one pair
+          else
+            assert(!"invalid");
         }
-        if (freqV.size() == 5)
+        else if (part2 && freq['J'] == 2)
         {
-          ret = 1; // high card;
+          if (freqV.size() == 2)
+            ret = 7; // five of a kind
+          else if (freqV.size() == 3)
+            ret = 6; // four of a kind
+          else if (freqV.size() == 4)
+            ret = 4; // three of akind
+          else
+            assert(!"invalid");
+        }
+        else if (part2 && freq['J'] == 3)
+        {
+          if (freqV.size() == 2)
+            ret = 7; // five of a kind
+          else if (freqV.size() == 3)
+            ret = 6; // four of a kind
+          else
+            assert(!"invalid");
+        }
+        else if (part2 && freq['J'] == 4)
+        {
+          ret = 7; // five of a kind
+        }
+        else if (part2 && freq['J'] == 5)
+        {
+          ret = 7; // five of a kind
+        }
+        else
+        {
+
+          //if (h[0] == h[1] == h[2] == h[3] == h[4])
+          if (freqV.size() == 1)
+            ret = 7; // five of a kind
+          if (freqV.size() == 2)
+          {
+            if (freqV[0].second == 4 || freqV[1].second == 4)
+              ret = 6; // four of a kind
+            if (freqV[0].second == 3 || freqV[1].second == 3)
+              ret = 5; // full house
+          }
+          if (freqV.size() == 3)
+          {
+            if (freqV[0].second == 3 || freqV[1].second == 3 || freqV[2].second == 3)
+              ret = 4; // three of akind
+
+            if (freqV[0].second == 2 || freqV[1].second == 2 || freqV[2].second == 2)
+              ret = 3; // two pair
+          }
+          if (freqV.size() == 4)
+          {
+            if (freqV[0].second == 2 || freqV[1].second == 2 || freqV[2].second == 2 || freqV[3].second == 2)
+              ret = 2;; // one pair
+          }
+          if (freqV.size() == 5)
+          {
+            ret = 1; // high card;
+          }
         }
 
         assert(ret != 0);
@@ -136,32 +196,29 @@ public:
     }
     return ret;
   }
-
-  LL DoWork2()
-  {
-    LL ret = 72;
-    return ret;
-  }
   
   string Part1() override
   {
     ReadData();
 
-    return std::to_string(DoWork1());
+    return std::to_string(DoWork(false));
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork2());
+    return std::to_string(DoWork(true));
   }
 
   bool Test() override
   {
     mCurrentInput = "test";
-    assert(Part1() != "");
-    //assert(Part2() != "");
+    aoc_assert(Part1(), "6440"s);
+    aoc_assert(Part2(), "5905"s);
+    mCurrentInput = "input";
+    aoc_assert(Part1(), "250254244"s);
+    aoc_assert(Part2(), "250087440"s);
     return true;
   }
 };
