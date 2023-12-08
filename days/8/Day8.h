6fb94ca6062f32086a7bf274031ce700a8ce4d6f
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2023-12-08 07:12:14 +0200

    asdas

--- days/8/Day8.h
+++ days/8/Day8.h
@@ -27,7 +27,64 @@ public:
 
   LL DoWork1()
   {
-    LL ret = 81;
+    LL ret = 0;
+    struct dd
+    {
+      string left, right;
+    };
+    map<string, dd> nodes;
+
+    string firstLine;
+    for (auto d : mData)
+    {
+      if (firstLine.empty())
+      {
+        firstLine = d;
+        continue;
+      }
+      if (d.empty())
+        continue;
+      auto [n, l, r] = RegExMatch3(d, R"((\w+) = \((\w+), (\w+)\))");
+      int i = 0;
+      dd d;
+      d.left = l;
+      d.right = r;
+      nodes[n] = d;
+    }
+
+    vector<string> nodeV;
+    for (auto [k, v] : nodes)
+    {
+      if (k.back() == 'A')
+        nodeV.push_back(k);
+    }
+
+    LL steps = 0;
+    int crtI = 0;
+    while (true)
+    {
+      for (auto node : nodeV)
+      {
+        while (node != "ZZZ")
+        {
+          bool goLeft = firstLine[crtI] == 'L';
+          if (goLeft)
+            node = nodes[node].left;
+          else
+            node = nodes[node].right;
+
+          steps++;
+
+          if (node == "ZZZ")
+            break;
+
+          crtI += 1;
+          if (crtI > firstLine.size() - 1)
+            crtI = 0;
+        }
+      }
+    }
+    ret = steps;
     return ret;
   }
 
