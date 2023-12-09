9fa3dff86be89b35c9e153a1a5362559c64fe36b
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2023-12-09 07:18:46 +0200

    Add solution for day9

--- days/9/Day9.h
+++ days/9/Day9.h
@@ -25,15 +25,44 @@ public:
     return "9";
   }
 
-  LL DoWork1()
+  LL DoWork(bool part2)
   {
-    LL ret = 91;
-    return ret;
-  }
+    LL ret = 0;
+    for (auto d : mData)
+    {
+      auto v = stoll(tok(d, ' '));
+      map<int, vector<LL>> diffs;
+      auto crtV = v;
+      int crtStep = 0;
+      diffs[crtStep++] = crtV;
+      while (true)
+      {
+        vector<LL> diff;
+        for (int i = 1; i < crtV.size(); ++i)
+          diff.push_back(crtV[i] - crtV[i - 1]);
+        diffs[crtStep++] = diff;
 
-  LL DoWork2()
-  {
-    LL ret = 92;
+        crtV = diff;
+        bool shouldBreak = true;
+        for (auto k : crtV)
+          if (k != 0)
+            shouldBreak = false;
+        if (shouldBreak)
+          break;
+      }
+      crtStep -= 1;
+      LL nextValue = 0;
+      while (crtStep > 0)
+      {
+        if (part2)
+          nextValue = diffs[crtStep - 1].front() - nextValue;
+        else 
+          nextValue = nextValue + diffs[crtStep - 1].back();
+
+        crtStep -= 1;
+      }
+      ret += nextValue;
+    }
     return ret;
   }
   
@@ -41,21 +70,21 @@ public:
   {
     ReadData();
 
-    return std::to_string(DoWork1());
+    return std::to_string(DoWork(false));
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork2());
+    return std::to_string(DoWork(true));
   }
 
   bool Test() override
   {
-    mCurrentInput = "test";
-    //assert(Part1() != "");
-    //assert(Part2() != "");
+    mCurrentInput = "input";
+    aoc_assert(Part1(), "1743490457"s);
+    aoc_assert(Part2(), "1053"s);
     return true;
   }
 };
