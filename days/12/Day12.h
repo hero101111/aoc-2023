3501b43b7f2dcaddd8f4da52da8ea0dd98ee4a46
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2023-12-12 07:48:56 +0200

    Solution for day 12 part 1

--- days/12/Day12.h
+++ days/12/Day12.h
@@ -14,10 +14,55 @@ public:
 
   ~Day12() override { }
 
+
+  int findQ(string p)
+  {
+    for (int i = 0; i < p.size(); ++i)
+      if (p[i] == '?') return i;
+    return -1;
+  }
+
+  bool evalP(string p, vector<LL> extra)
+  {
+    int crtFound = 0;
+
+    if (findQ(p) >= 0)
+      return false;
+
+    p += "E";
+    for (auto c : p)
+    {
+      if (c == '#')
+      {
+        crtFound++;
+      }
+      if (c == '.' || c == 'E')
+      {
+        if (crtFound > 0 && extra.empty())
+          return false;
+
+        if (crtFound > 0 && extra.front() != crtFound)
+          return false;
+
+        if (crtFound > 0 && !extra.empty())
+          extra.erase(begin(extra));
+        
+        crtFound = 0;
+      }
+    }
+    if (extra.size() > 0)
+      return false;
+
+    return true;
+  }
+
+
   void ReadData()
   {
     mData.clear();
     mData = rff(GetInputPath());
+
+    
   }
   
   string GetDay() override
@@ -27,8 +72,61 @@ public:
 
   LL DoWork1()
   {
-    LL ret = 121;
-    return ret;
+    LL t = 0;
+
+    auto b = evalP(".#.###.#.######"s, vector<LL>{1, 3, 1, 6});
+
+  // auto b2 = evalP(".??..??...?##.", vector<LL>{ 1, 1, 3});
+    auto b2 = evalP("..#...#...###.", vector<LL>{ 1, 1, 3});
+    for (auto d : mData)
+    {
+      auto [pattern, extra] = split(d, ' ');
+      auto extraV = stoll(tok(extra, ','));
+      map<int, int> pm;
+      int totalQ = 0;
+      for (int i = 0; i < pattern.size(); ++i)
+        if (pattern[i] == '?')
+          pm[totalQ++] = i;
+
+      stack<string> crtS;
+      crtS.push(pattern);
+
+      while (true)
+      {
+        auto crtTop = crtS.top();
+
+
+        if (evalP(crtTop, extraV))
+        {
+         // cout << "YO   " << crtTop << endl;
+          t++;
+        }
+
+        crtS.pop();
+
+        auto firstQ = findQ(crtTop);
+        if (firstQ >= 0)
+        {
+          string newPattern1 = crtTop;
+          string newPattern2 = crtTop;
+          /*  if (crtTop.second < totalQ)
+              newPattern[pm[crtTop.second]] = '#';*/
+
+          auto it = crtTop.find('?');
+          if (it != string::npos)
+          {
+            newPattern1[firstQ] = '#';
+            newPattern2[firstQ] = '.';
+            crtS.push(newPattern1);
+            crtS.push(newPattern2);
+          }
+        }
+
+        if (crtS.empty())
+          break;
+      }
+    }
+    return t;
   }
 
   LL DoWork2()
@@ -54,7 +152,7 @@ public:
   bool Test() override
   {
     mCurrentInput = "test";
-    //assert(Part1() != "");
+   // assert(Part1() != "");
     //assert(Part2() != "");
     return true;
   }
