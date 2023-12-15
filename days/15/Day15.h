af54be9bfa013edfdd96157ebdb9b03c5f2343d2
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2023-12-15 07:51:24 +0200

    Add day 15 solution

--- days/15/Day15.h
+++ days/15/Day15.h
@@ -5,38 +5,125 @@
 class Day15 : public ISolutionDay
 {
 private:
-
   vector<string> mData;
 
 public:
+  Day15() {}
 
-  Day15(){ }
-
-  ~Day15() override { }
+  ~Day15() override {}
 
   void ReadData()
   {
     mData.clear();
     mData = rff(GetInputPath());
   }
-  
-  string GetDay() override
+
+  string GetDay() override { return "15"; }
+
+  int hash(string t)
   {
-    return "15";
+    int s = 0;
+    for (char c : t)
+    {
+      int asc = c;
+      s += asc;
+      s *= 17;
+      s = s % 256;
+    }
+    return s;
   }
+
+  struct lens
+  {
+    string label;
+    int    focal;
+  };
 
   LL DoWork1()
   {
-    LL ret = 151;
+    LL ret = 0;
+
+    auto d = mData[0];
+    // d       = "HASH";
+    auto tk = tok(d, ',');
+    for (auto t : tk)
+    {
+      ret += hash(t);
+    }
+
     return ret;
   }
 
   LL DoWork2()
   {
-    LL ret = 152;
+    LL ret = 0;
+
+    map<int, vector<lens>> m;
+    for (auto d : mData)
+    {
+      // d       = "HASH";
+      auto tk = tok(d, ',');
+      for (auto t : tk)
+      {
+        string t1, t2, minus;
+
+        if (t.contains('-'))
+          minus = t.substr(0, t.size() - 1);
+        else
+        {
+          auto p = split(t, '=');
+          t1     = p.first;
+          t2     = p.second;
+        }
+
+        if (!t1.empty())
+        {
+          int box = hash(t1);
+
+          auto & b  = m[box];
+          auto   it = find_if(begin(b), end(b),
+                              [t1](lens & bx)
+                              {
+                              return bx.label == t1;
+                            });
+          if (it != end(b))
+            it->focal = stoi(t2);
+          else
+            b.push_back(lens{ t1, stoi(t2) });
+        }
+        else if (!minus.empty())
+        {
+          int    box = hash(minus);
+          auto & b   = m[box];
+          auto   it  = find_if(begin(b), end(b),
+                               [minus](lens & bx)
+                               {
+                              return bx.label == minus;
+                            });
+          if (it != end(b))
+            b.erase(it);
+        }
+        else
+          assert(!"invalid");
+      }
+      break;
+    }
+
+    for (auto b : m)
+    {
+      for (int li = 0; li < b.second.size(); ++li)
+      {
+        int    bn    = b.first + 1;
+        int    sn    = li + 1;
+        int    fp    = b.second[li].focal;
+        string label = b.second[li].label;
+        int    r     = bn * sn * fp;
+        ret += r;
+      }
+    }
     return ret;
   }
-  
+
   string Part1() override
   {
     ReadData();
@@ -53,9 +140,9 @@ public:
 
   bool Test() override
   {
-    mCurrentInput = "test";
-    //assert(Part1() != "");
-    //assert(Part2() != "");
+    mCurrentInput = "input";
+    aoc_assert(Part1(), "517965"s);
+    aoc_assert(Part2(), "267372"s);
     return true;
   }
 };
