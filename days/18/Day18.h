71a0cf812646886f4a8efb8ad751ded44004e4e3
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2023-12-18 07:29:25 +0200

    Day 18 part 1

--- days/18/Day18.h
+++ days/18/Day18.h
@@ -5,30 +5,88 @@
 class Day18 : public ISolutionDay
 {
 private:
-
   vector<string> mData;
 
 public:
+  Day18() {}
 
-  Day18(){ }
-
-  ~Day18() override { }
+  ~Day18() override {}
 
   void ReadData()
   {
     mData.clear();
     mData = rff(GetInputPath());
   }
-  
-  string GetDay() override
-  {
-    return "18";
-  }
+
+  string GetDay() override { return "18"; }
 
   LL DoWork1()
   {
-    LL ret = 181;
-    return ret;
+    LL ret = 0;
+
+    DynamicMap<char> m;
+    Point            p;
+    m[p] = '#';
+    for (auto d : mData)
+    {
+      auto v   = tok(d, ' ');
+      char dir = v[0][0];
+      int  len = stoi(v[1]);
+      // color
+
+      for (int i = 0; i < len; ++i)
+      {
+        p    = p.FromDirection(dir);
+        m[p] = '#';
+      }
+    }
+
+    // Point                fillP{ 190, 66 };
+
+    Point fillP;
+    m.for_each(
+      [&](Point p, char c)
+      {
+        if (fillP == Point{})
+          fillP = p;
+      });
+
+    fillP = fillP + Point{ 1, 1 };
+
+    //  m[fillP] = 'A';
+
+    unordered_set<Point> toFlood;
+    toFlood.insert(fillP);
+
+    // m.printf(KVERBOSE);
+
+    while (true)
+    {
+      unordered_set<Point> shouldFlood;
+      for (auto p : toFlood)
+      {
+        for (auto cand : p.GetDirectNeighbours())
+        {
+          if (!m.isSetAt(cand))
+          {
+            m[cand] = '#';
+            shouldFlood.insert(cand);
+          }
+        }
+      }
+      toFlood = shouldFlood;
+
+      //  m.printf(KVERBOSE);
+      if (toFlood.empty())
+        break;
+    }
+
+    //   m.printf(KVERBOSE);
+    return m.count_if(
+      [](Point p, char c)
+      {
+        return c == '#';
+      });
   }
 
   LL DoWork2()
@@ -36,7 +94,7 @@ public:
     LL ret = 182;
     return ret;
   }
-  
+
   string Part1() override
   {
     ReadData();
@@ -54,8 +112,8 @@ public:
   bool Test() override
   {
     mCurrentInput = "test";
-    //assert(Part1() != "");
-    //assert(Part2() != "");
+    // assert(Part1() != "");
+    // assert(Part2() != "");
     return true;
   }
 };
